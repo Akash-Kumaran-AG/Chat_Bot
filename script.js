1d69f97b228ad4d7204581cce3589cd1be51ced3
Akash-Kumaran-AG <161023198+Akash-Kumaran-AG@users.noreply.github.com>
2024-03-30 22:55:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+const messageBar = document.querySelector(".bar-wrapper input");
+const sendBtn = document.querySelector(".bar-wrapper button");
+const messageBox = document.querySelector(".message-box");
+
+let API_URL = "https://api.openai.com/v1/chat/completions";
+let API_KEY = 'API KEY'
+
+sendBtn.onclick = function () {
+  if(messageBar.value.length > 0){
+    const UserTypedMessage = messageBar.value;
+    messageBar.value = "";
+
+    let message =
+    `<div class="chat message">
+    <img src="gpt image.jpg">
+    <span>
+      ${UserTypedMessage}
+    </span>
+  </div>`;
+
+  let response =
+  `<div class="chat response">
+  <img src="gpt image.jpg">
+  <span class= "new">"Love" is a complex and multifaceted emotion that humans experience. It can be defined in various ways depending on cultural, philosophical, 
+  and personal perspectives. Generally, love encompasses feelings of deep affection, care, and attachment towards someone or 
+  something. It can manifest in different forms such as romantic love, platonic love, familial love, and compassionate love.
+  </span>
+</div>`
+
+    messageBox.insertAdjacentHTML("beforeend", message);
+
+    setTimeout(() =>{
+      messageBox.insertAdjacentHTML("beforeend", response);
+
+      const requestOptions = {
+        method : "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+          "model": "gpt-3.5-turbo",
+          "messages": [{"role": "user", "content": UserTypedMessage}]
+        })
+      }
+
+      fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        const ChatBotResponse = document.querySelector(".response .new");
+        ChatBotResponse.innerHTML = data.choices[0].message.content;
+        ChatBotResponse.classList.remove("new");
+      }).catch((error) => {
+        ChatBotResponse.innerHTML = "Opps! An error occured. Please try again"
+      })
+    }, 100);
+  }
+}
